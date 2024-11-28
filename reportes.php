0a3cd540fdcfc3880077bf0021db868d685c5630
Asul003 <jorgesaulbm.2003@gmail.com>
2024-11-27 23:30:48 -0600

    Categorias
    Se agrego para corregir categorias

--- reportes.php
+++ reportes.php
@@ -113,6 +113,106 @@ mysqli_close($conexion);
                 </table>
             </div>
         </section>
+
+        <section class="categorias my-4">
+            <div class="container d-block">
+                <h1>Categorias</h1>
+                <!-- Botón para abrir el modal -->
+                <button type="button" class="color-btn mt-2 mb-2" data-bs-toggle="modal"
+                    data-bs-target="#crearCategoriaModal">
+                    Crear Categoría
+                </button>
+
+                <!-- Modal -->
+                <div class="modal fade" id="crearCategoriaModal" tabindex="-1" aria-labelledby="crearCategoriaLabel"
+                    aria-hidden="true">
+                    <div class="modal-dialog">
+                        <div class="modal-content bg-dark">
+                            <div class="modal-header">
+                                <h5 class="modal-title" id="crearCategoriaLabel">Crear Categoría</h5>
+                                <button type="button" class="btn-close" data-bs-dismiss="modal"
+                                    aria-label="Close"></button>
+                            </div>
+                            <div class="modal-body">
+                                <form id="formCrearCategoria" method="post" action="controladores/CrearCategoria.php">
+                                    <div class="mb-3">
+                                        <label for="nombreCategoria" class="form-label">Nombre</label>
+                                        <input type="text" class="form-control px-3" id="nombreCategoria" name="nombre"
+                                            required>
+                                    </div>
+                                    <div class="mb-3">
+                                        <label for="descripcionCategoria" class="form-label">Descripción</label>
+                                        <textarea class="form-control" id="descripcionCategoria" name="descripcion"
+                                            rows="3" required></textarea>
+                                    </div>
+                                    <button type="submit" class="color-btn">Guardar</button>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+
+
+                <div class="mt-4">
+                    <ul class="categorias list-group mb-2">
+                        <?php foreach ($categorias as $categoria): ?>
+                            <li id="cat-<?php echo $categoria->categoriaID; ?>"
+                                class="mb-2 categoria list-group-item bg-transparent text-white d-flex border-1 border-white">
+                                <div class="text-wrapper flex-grow-1">
+                                    <h3> <?php echo htmlspecialchars($categoria->nombre); ?></h3>
+                                    <p class="m-0"> <?php echo htmlspecialchars($categoria->descripcion); ?></p>
+                                </div>
+                                <div class="boton align-items-center d-flex">
+                                    <button data-id="<?php echo $categoria->categoriaID; ?>" class="color-btn  mx-2"
+                                        data-bs-toggle="modal"
+                                        data-bs-target="#editarCategoriaModal-<?php echo $categoria->categoriaID; ?>">Editar</button>
+                                    <a onclick="return confirmarBorrado()"
+                                        href="./controladores/borrarCategoria.php?id=<?php echo $categoria->categoriaID; ?>"
+                                        class="color-btn">Borrar
+                                    </a>
+                                </div>
+                            </li>
+
+                            <div class="modal fade" id="editarCategoriaModal-<?php echo $categoria->categoriaID; ?>"
+                                tabindex="-1" aria-labelledby="editarCategoriaModal" aria-hidden="true">
+                                <div class="modal-dialog">
+                                    <div class="modal-content bg-dark">
+                                        <div class="modal-header">
+                                            <h5 class="modal-title" id="editarCategoriaLabel">Editar Categoría</h5>
+                                            <button type="button" class="btn-close" data-bs-dismiss="modal"
+                                                aria-label="Close"></button>
+                                        </div>
+                                        <div class="modal-body">
+                                            <form id="formEditarCategoria" method="post"
+                                                action="./controladores/EditarCategoria.php?">
+                                                <input type="hidden" name="categoriaID"
+                                                    value="<?php echo $categoria->categoriaID; ?>">
+                                                <div class="mb-3">
+                                                    <label for="nombreCategoria" class="form-label">Nombre</label>
+                                                    <input type="text" class="form-control px-3" id="nombreCategoria"
+                                                        name="nombre" required value="<?php echo $categoria->nombre; ?>">
+                                                </div>
+                                                <div class="mb-3">
+                                                    <label for="descripcionCategoria" class="form-label">Descripción</label>
+                                                    <textarea class="form-control" id="descripcionCategoria"
+                                                        name="descripcion" rows="3"
+                                                        required><?php echo $categoria->descripcion; ?></textarea>
+                                                </div>
+                                                <button type="submit" class="color-btn">Guardar</button>
+                                            </form>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        <?php endforeach; ?>
+                    </ul>
+                </div>
+            </div>
+        </section>
+
+
+
     </main>
 
 </body>
