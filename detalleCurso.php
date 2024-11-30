e63a8fa2acc1f7ad94f6ef3534a9f4a82c4b6562
Sora <100179310+SoraIsHere@users.noreply.github.com>
2024-11-29 22:06:49 -0600

    titles

--- detalleCurso.php
+++ detalleCurso.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Reporte de Usuarios</title>
+    <title>Pagina de curso</title>
     <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="css/curso.css">
 </head>
